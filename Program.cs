816dbe5649c0b11475c1d36b7f0cc77a5f87a5cf
Randy Moya <stevenmoya02@gmail.com>
2023-09-02 22:14:08 -0600

    Se movio el el archivo Books.json para poder lerr sus datos, ademas se escribio codigo en LinqQueries para poder convertir los datos de Books.json en una coleccion y retornar esa coleccion para poder imprmirla en Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+LinqQueries queries = new LinqQueries();
+
+printValues(queries.allCollection());
+
+void printValues(IEnumerable<Book> booksList)
+{
+    Console.WriteLine("{0,-70} {1,7} {2,11}\n", "Title", "Pages", "Published date" );
+    foreach (var item in booksList)
+    {
+        Console.WriteLine("{0,-70} {1,7} {2,11}",item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
